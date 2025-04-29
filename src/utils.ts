1e8385f6e1f92676ed86ae9a7ff459fd7af8ae17
Gregory Oschwald <goschwald@maxmind.com>
2025-04-29 13:31:20 -0700

    Use readUInt*BE functions
    Although there is probalby some performance gain from this, it is
    quite minimal. The primary benefit is the reduction in code.

--- src/utils.ts
+++ src/utils.ts
@@ -1,15 +1,3 @@
-const concat2 = (a: number, b: number): number => {
-  return (a << 8) | b;
-};
-
-const concat3 = (a: number, b: number, c: number): number => {
-  return (a << 16) | (b << 8) | c;
-};
-
-const concat4 = (a: number, b: number, c: number, d: number): number => {
-  return (a << 24) | (b << 16) | (c << 8) | d;
-};
-
 const legacyErrorMessage = `Maxmind v2 module has changed API.\n\
 Upgrade instructions can be found here: \
 https://github.com/runk/node-maxmind/wiki/Migration-guide\n\
@@ -23,8 +11,5 @@ const assert = (condition: boolean, message: string): void => {
 
 export default {
   assert,
-  concat2,
-  concat3,
-  concat4,
   legacyErrorMessage,
 };
